414a5ef20efdafbd3ef55cbfac2ced65af4def6f
noviaretno <32433423+noviaretno@users.noreply.github.com>
2018-01-18 06:58:39 -0800

    meolah public

--- classmusang.cpp
+++ classmusang.cpp
@@ -7,3 +7,13 @@ class jenismusang{
 		string jenismakanan;
 		string warnabulu;
 		int panjangtubuh;
+	public:
+		jenismusang (string namamusang, string makanan, string warnabulu, int panjangtubuh )
+		{
+			jenismusang::namamusang = namamusang;
+			jenismusang::jenismakanan = makanan;
+			jenismusang::warnabulu = warnabulu;
+			jenismusang:: panjangtubuh = panjangtubuh;
+			
+		
+		}
